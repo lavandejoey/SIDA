3680cf6eb15507f52d7dae6842dcf24b258650a8
zhenglin huang <147322689+hzlsaber@users.noreply.github.com>
2025-05-27 17:30:48 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-deepspeed --include localhost:1 --master_port=24999 test.py \
+deepspeed --include localhost: --master_port=24999 test.py \
   --version="./ck/SIDA-7B" \
   --dataset_dir='/path/to/benchmark/' \
   --vision_pretrained="./ck/sam_vit_h_4b8939.pth" \
